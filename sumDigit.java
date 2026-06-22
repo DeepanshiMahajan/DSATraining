c6a54cafe115158322f9576be7950497bd9b5773
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-23 00:23:53 +0530

    sum of digits in no

--- sumDigit.java
+++ sumDigit.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class sumDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
         int n = sc.nextInt();
         int sum = 0;
 
-        while (n > 0) {
-            int digit = n % 10; 
-            sum += digit;
-            n = n / 10;         
+        while( n > 0){
+            int lastDigit = n %10;
+            sum+=lastDigit;
+            n = n/10;
         }
         System.out.println(sum);
     }
