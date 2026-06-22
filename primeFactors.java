ea6e32383bcf412560fe75895286963fc73a42fe
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-23 00:31:52 +0530

    Prime factors of no

--- primeFactors.java
+++ primeFactors.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class primeFactors {
     public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
         int n =150;
+
         for(int i=2; i<=n; i++){
             while(n % i == 0){
                 System.out.println(i+ " ");
-                 n = n/i;
+                n = n/i;
             }
         }
     }
