961e18609ee7408e779e93d508fc593f73a9bfac
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-22 23:57:05 +0530

    Prime No

--- primeNo.java
+++ primeNo.java
@@ -4,16 +4,16 @@ public class primeNo {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Enter number :");
         int n = sc.nextInt();
         boolean t = true;
 
-        if (n <= 1) {
-            t = false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    t = false;
+        if( n <= 1){
+            t =false;
+        }else{
+            for(int i =2; i<=Math.sqrt(i);i++){
+                if( n % i == 0){
+                    t=false;
                     break;
                 }
             }
