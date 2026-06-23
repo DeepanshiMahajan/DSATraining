f83baf2bdb5b8e3fd04b561c239bc8c9578f2d50
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-23 23:03:43 +0530

    HCF OF NO

--- Day2/hcf.java
+++ Day2/hcf.java
@@ -3,11 +3,12 @@ public class hcf {
         int a = 12, b = 40;
         int hcf = 1;
 
-        for (int i = 1; i <= Math.min(a, b); i++) {
-            if (a % i == 0 && b % i == 0) {
-                hcf = i;
+        for(int i =1; i<=Math.min(a, b); i++){
+            if( a % i == 0 && b % i == 0){
+                hcf =i;
             }
         }
+        
 
         System.out.println("HCF = " + hcf);
     }
--- Day2/pattern3.java
+++ Day2/pattern3.java
@@ -31,3 +31,4 @@ public class pattern3 {
         }
     }
 }
+
