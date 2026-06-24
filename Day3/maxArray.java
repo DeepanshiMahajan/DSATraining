3021cfa291dfa42ba0af233dccf79c0d3a39cbc3
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-25 00:04:44 +0530

    Maximum of Array

--- Day3/maxArray.java
+++ Day3/maxArray.java
@@ -5,12 +5,12 @@ public class maxArray {
     public static void main(String[] args) {
         int arr[] = {0, 1, 5, 1, 3};
 
-        int maxOfArr = 0;
+        int max =arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
-            maxOfArr = Math.max(maxOfArr,arr[i]);
+        for(int i=1; i<arr.length; i++){
+            max = Math.max(max, arr[i]);
         }
 
-        System.out.println(maxOfArr);
+        System.out.println(max);
     }
 }
