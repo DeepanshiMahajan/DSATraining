c9af0222a9baff0856ed2473bf6cea6a428b7c83
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-25 00:12:47 +0530

    Maximum Freq Array

--- Day3/maxFreqNo.java
+++ Day3/maxFreqNo.java
@@ -3,17 +3,17 @@ package Day3;
 public class maxFreqNo {
     public static void main(String[] args) {
         int[] nums = { 6, 1, 3, 1, 2, 1, 1, 3 };
-        int ans = -1;
-        int maxFreq = 0;
+        int maxFreq  =0;
+        int ans =-1;
 
-        for (int i = 0; i < nums.length; i++) {
-            int count = 1;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
+        for(int i=0;i<nums.length;i++){
+            int count =1;
+            for(int j= i+1; j<nums.length; j++){
+                if(nums[i] == nums[j]){
                     count++;
                 }
             }
-            if (count > maxFreq) {
+            if( count > maxFreq){
                 maxFreq = count;
                 ans = nums[i];
             }
