c572151d0cef67e6f3bab3cc524b564c9b2a015a
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-25 00:34:32 +0530

    Second Largest

--- Day3/secLargest.java
+++ Day3/secLargest.java
@@ -3,8 +3,8 @@ package Day3;
 public class secLargest {
     public static void main(String[] args) {
         int n = 96752;
-        int firstLar = -1;
-        int secLargest = -1;
+        int firstLar = 0;
+        int secLargest = 0;
 
         while(n > 0){
            int ld = n%10;
